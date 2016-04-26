cab6edaad148cc75079c31964220b5fa411e98dc
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-04-26 14:54:53 -0400

    Updated from Brython Server: 4/26/2016 2:54:53 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,9 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+s = input("Please enter a string of text ")
+
+print(s[::-1])
+
+s = s.split(' ')
+print(s[::-1])
